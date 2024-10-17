46dc6bcd1ae9264dc98ef831366752794ac78852
Prince Gupta <prince356gupta@gmail.com>
2024-10-17 20:23:01 +0530

    Add files via upload

---
+++ ViewModels/ClientLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OleHotels.Models
+{
+    public class ClientLoginViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(30)]
+        public string Password { get; set; }
+    }
+}
